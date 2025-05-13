e95f09f302cd6fbfb571573b7d8da6806dc9d369
itssadiq <sadiqalishaikh009@gmail.com>
2025-05-13 12:59:36 +0500

    - Made add button interactive, shows dialogue box upon click. - Dialogue box can be closed as well. - Rest of the changes are to make the overall layout work.

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
-function addNote() {
+const notes = [
+  {
+    title: "Note 01",
+    category: "Test",
+    content: "some content here",
+  },
+];
+
+loadPage();
+
+function loadPage() {
+  const noteForm = document.querySelector(".note-form");
+  const overlay = document.getElementById("overlay");
+
+  showNotesInput(noteForm, overlay);
+  closeNotesInput(noteForm, overlay);
+
+  document.querySelector(".js-save-button").addEventListener("click", addNote);
+}
+
+function showNotesInput(noteForm, overlay) {
   const addButton = document.querySelector(".js-add-btn");
 
   addButton.addEventListener("click", operation);
 
   function operation() {
-    console.log("clicked");
+    overlay.classList.add("active");
+    noteForm.classList.add("active");
   }
 }
 
-addNote();
+function closeNotesInput(noteForm, overlay) {
+  const closeButton = document.querySelector(".close-notes-input");
+
+  closeButton.addEventListener("click", operation);
+
+  function operation() {
+    overlay.classList.remove("active");
+    noteForm.classList.remove("active");
+  }
+}
+
+function addNote() {
+  const title = document.querySelector(".js-title-input").value;
+  const category = document.querySelector(".js-category-input").value;
+  const content = document.querySelector(".js-content-input").value;
+
+  notes.push({
+    title,
+    category,
+    content,
+  });
+
+  console.log(notes);
+}
