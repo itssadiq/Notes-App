e06a684eb955c768adb56928c082d6f0ffe7a1ec
itssadiq <sadiqalishaikh009@gmail.com>
2025-12-06 11:28:16 +0500

    Starter code for frontend/backend.

---
+++ server/src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server started at port 3000");
+});
